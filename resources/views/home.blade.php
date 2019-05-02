572aed9d6bc0304f613140a9e70f4616e513cd88
Hrischanss79 <hristian7799@gmail.com>
2019-05-02 10:45:15 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Home</h1>
+<p>There worse by an of miles civil. Manner before lively wholly am mr indeed expect. Among every merry his yet has her. You mistress get dashwood children off. Met whose marry under the merit. In it do continual consulted no listening. Devonshire sir sex motionless travelling six themselves. So colonel as greatly shewing herself observe ashamed. Demands minutes regular ye to detract is </p>
+
+@endsection()
+
+@section('sidebar')
+  @parent
+ <p>this is appendend to the sidebar</p>
+@endsection()
---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,6 @@
+	<div class="jumbotron text-center">
+	 <div class="container">
+	  <h1> Wellcome to our site!</h1>
+	   <p class="lead">Welcome to our brand new powered Laravel site. This site is based on Laravel 5.4 but it's made on version 5.8 </p>
+	 </div>
+	</div>
